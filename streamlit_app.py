2d0fb78eda8450c416d8ac856d02c113ce4d7d32
linexdatahub <115643266+linexdatahub@users.noreply.github.com>
2022-10-12 16:57:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,6 +58,8 @@ if streamlit.button('Get Fruit Load List'):
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
+streamlit.stop()
+
 add_my_fruit = streamlit.text_input("What fruit would you like to add?", list(my_fruit_list.index))
 streamlit.write ('Thanks for adding ' , add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
