0c624d77389e78fc2ac9a47a534b8966e7974041
linexdatahub <115643266+linexdatahub@users.noreply.github.com>
2022-10-12 16:55:54 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,14 +43,20 @@ try:
 except URLError as e:
     streamlit.error()
 
+def get_fruit_load_list():
+        with my_cnx.cursor() as my_cur:
+             my_cur.execute("use warehouse compute_wh")
+             my_cur.execute("select * from fruit_load_list")
+             return my_cur.fetchall()
+
+if streamlit.button('Get Fruit Load List'):
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_data_rows = get_fruit_load_lists()
+streamlit.datafram(my_data_rows)
+
+
 streamlit.stop()
 
-
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("use warehouse compute_wh")
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_rows)
 
